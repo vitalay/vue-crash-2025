c8fd52d7275bfef8a62fa42c454036f895c766a4
vitalya <vitalay.efimov@gmail.com>
2025-01-21 19:26:23 +0500

    сделал кастомный Class

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
 import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
 
 </script>
 
 <template>
 
+  
   <Navbar />
-  <Hero title="Test Title" subtitle="Test Subtitle" />
+  <Hero   />
+  <HomeCards />
 </template>
