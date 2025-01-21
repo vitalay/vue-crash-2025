271f12512c618aff1011453730d1dbcc9c1766f0
vitalya <vitalay.efimov@gmail.com>
2025-01-21 20:29:06 +0500

    подключил JSON

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,12 @@
 import Navbar from '@/components/Navbar.vue';
 import Hero from '@/components/Hero.vue';
 import HomeCards from '@/components/HomeCards.vue';
-
+import JobsListings from '@/components/JobsListings.vue';
 </script>
 
 <template>
-
-  
   <Navbar />
   <Hero   />
   <HomeCards />
+  <JobsListings />
 </template>
