bcf21c214a87aeb71c14a49b4459e452bd9c13e6
vitalya <vitalay.efimov@gmail.com>
2025-01-21 21:04:08 +0500

    добавил списки вакансий JSON

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 import Navbar from '@/components/Navbar.vue';
 import Hero from '@/components/Hero.vue';
 import HomeCards from '@/components/HomeCards.vue';
-import JobsListings from '@/components/JobsListings.vue';
+import JobListings from '@/components/JobListings.vue';
 </script>
 
 <template>
   <Navbar />
   <Hero   />
   <HomeCards />
-  <JobsListings />
+  <JobListings />
 </template>
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,6 +1,8 @@
 <script setup>
+import  JobListing  from './JobListing.vue';
 import jobData from '@/jobs.json';
 import { ref } from 'vue';
+
 
 const jobs = ref(jobData);
 
@@ -11,13 +13,10 @@ const jobs = ref(jobData);
     <div class="container-xl lg:container m-auto">
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-          <div v-for="job in jobs" :key="job.id">
-            {{ job.title }}
-          </div>
-        </div>
       </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+      </div>
     </div>
   </section>
-
 </template>
