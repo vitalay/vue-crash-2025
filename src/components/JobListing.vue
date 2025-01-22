483986853ad4d3893a3cffaa0685c2eb40486baf
vitalya <vitalay.efimov@gmail.com>
2025-01-22 19:41:42 +0500

    Настроил роуторы

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import { RouterLink } from 'vue-router';
 import { defineProps, ref, computed } from 'vue';
 
 const props = defineProps({
@@ -48,10 +48,10 @@ const truncatedDescription = computed(() => {
           <i class="pi pi-map-marker text-orange-700"></i>
           {{ job.location }}
         </div>
-        <a :href="'/job/' + job.id"
+        <RouterLink :to="'/job/' + job.id"
           class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm">
           Read More
-        </a>
+        </RouterLink>
       </div>
     </div>
   </div>
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,4 +1,5 @@
 <script setup>
+import {RouterLink} from 'vue-router';
 import  JobListing  from './JobListing.vue';
 import jobData from '@/jobs.json';
 import { ref,  defineProps } from 'vue';
@@ -27,7 +28,7 @@ const jobs = ref(jobData);
     </div>
   </section>
   <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-    <a href="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View All
-      Jobs</a>
+    <RouterLink to="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View All
+      Jobs</RouterLink>
   </section>
 </template>
