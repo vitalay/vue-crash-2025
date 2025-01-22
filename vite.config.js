f88fa29ef25d73867dc8ca5743bb3368bba5a913
vitalya <vitalay.efimov@gmail.com>
2025-01-22 19:30:51 +0500

    нвстроил роутер NAVBAR

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue-crash-2025/',
   plugins: [
     vue(),
     vueDevTools(),
