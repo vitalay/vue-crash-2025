76f40335c40be18cc96601146717b0a5e619c54d
vitalya <vitalay.efimov@gmail.com>
2025-01-22 20:55:43 +0500

    router  страница с ошибкой

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import JobsView from '@/views/JobsView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 
 const router = createRouter({
@@ -15,6 +16,11 @@ const router = createRouter({
 			path: '/jobs',
 			name: 'jobs',
 			component: JobsView,
+		},
+		{
+			path: '/:catchAll(.*)',
+			name: 'not-found',
+			component: NotFoundView,
 		},
 	],
 })
